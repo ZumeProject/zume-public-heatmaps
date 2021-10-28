71591e2a5077a8dc9e76583a060f9744941590cd
corsacca <corsacca@gmail.com>
2021-10-28 14:37:09 +0100

    Faster query_saturation_list function

--- magic/heatmap.php
+++ magic/heatmap.php
@@ -18,81 +18,29 @@ class Zume_App_Heatmap {
             return $value;
         }
 
-        // 44141 records
+        $country_only = [ 100000000, 100001073, 100024783, 100047360, 100054604, 100056004, 100056005, 100089588, 100131318, 100222390, 100238556, 100241760, 100243745, 100245356, 100247244, 100253792, 100254764, 100260851, 100341224, 100343035, 100367575, 100380097 ];
+        # admin 1 for little highly divided countries
+        #'Romania', 'Estonia', 'Bhutan', 'Croatia', 'Solomon Islands', 'Guyana', 'Iceland', 'Vanuatu', 'Cape Verde', 'Samoa', 'Faroe Islands', 'Norway', 'Uruguay', 'Mongolia', 'United Arab Emirates', 'Slovenia', 'Bulgaria', 'Honduras', 'Columbia', 'Namibia', 'Switzerland', 'Western Sahara'
+        $admin_1_countries = [ 100314737,100083318,100041128,100133112,100341242,100132648,100222839,100379914,100055707,100379993,100130389,100255271,100363975,100248845,100001527,100342458,100024289,100132795,100054605,100253456,100342975,100074571 ];
+        # admin 3 for big countries
+        #'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
+        $admin_3_countries = [ 100050711, 100219347, 100089589, 100074576, 100259978, 100018514 ];
 
         global $wpdb;
-        $results = $wpdb->get_results("
-
-            SELECT
-            lg1.grid_id, lg1.population, lg1.country_code
-            FROM $wpdb->dt_location_grid lg1
-            WHERE lg1.level = 0
-			AND lg1.grid_id NOT IN ( SELECT lg11.admin0_grid_id FROM $wpdb->dt_location_grid lg11 WHERE lg11.level = 1 AND lg11.admin0_grid_id = lg1.grid_id )
- 			#'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
-            AND lg1.admin0_grid_id NOT IN (100050711,100219347, 100089589,100074576,100259978,100018514)
-            #'Romania', 'Estonia', 'Bhutan', 'Croatia', 'Solomon Islands', 'Guyana', 'Iceland', 'Vanuatu', 'Cape Verde', 'Samoa', 'Faroe Islands', 'Norway', 'Uruguay', 'Mongolia', 'United Arab Emirates', 'Slovenia', 'Bulgaria', 'Honduras', 'Columbia', 'Namibia', 'Switzerland', 'Western Sahara'
-            AND lg1.admin0_grid_id NOT IN (100314737,100083318,100041128,100133112,100341242,100132648,100222839,100379914,100055707,100379993,100130389,100255271,100363975,100248845,100001527,100342458,100024289,100132795,100054605,100253456,100342975,100074571)
-			# above admin 0 (22)
-
-			UNION ALL
-            --
-            # admin 1 locations that have no level 2 (768)
-            --
-            SELECT
-            lg2.grid_id, lg2.population, lg2.country_code
-            FROM $wpdb->dt_location_grid lg2
-            WHERE lg2.level = 1
-			AND lg2.grid_id NOT IN ( SELECT lg22.admin1_grid_id FROM $wpdb->dt_location_grid lg22 WHERE lg22.level = 2 AND lg22.admin1_grid_id = lg2.grid_id )
-             #'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
-            AND lg2.admin0_grid_id NOT IN (100050711,100219347, 100089589,100074576,100259978,100018514)
-            #'Romania', 'Estonia', 'Bhutan', 'Croatia', 'Solomon Islands', 'Guyana', 'Iceland', 'Vanuatu', 'Cape Verde', 'Samoa', 'Faroe Islands', 'Norway', 'Uruguay', 'Mongolia', 'United Arab Emirates', 'Slovenia', 'Bulgaria', 'Honduras', 'Columbia', 'Namibia', 'Switzerland', 'Western Sahara'
-            AND lg2.admin0_grid_id NOT IN (100314737,100083318,100041128,100133112,100341242,100132648,100222839,100379914,100055707,100379993,100130389,100255271,100363975,100248845,100001527,100342458,100024289,100132795,100054605,100253456,100342975,100074571)
-
-			UNION ALL
-			--
-            # admin 2 all countries (37100)
-            --
-			SELECT
-            lg3.grid_id, lg3.population,  lg3.country_code
-            FROM $wpdb->dt_location_grid lg3
-            WHERE lg3.level = 2
-            #'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
-            AND lg3.admin0_grid_id NOT IN (100050711,100219347, 100089589,100074576,100259978,100018514)
-            #'Romania', 'Estonia', 'Bhutan', 'Croatia', 'Solomon Islands', 'Guyana', 'Iceland', 'Vanuatu', 'Cape Verde', 'Samoa', 'Faroe Islands', 'Norway', 'Uruguay', 'Mongolia', 'United Arab Emirates', 'Slovenia', 'Bulgaria', 'Honduras', 'Columbia', 'Namibia', 'Switzerland', 'Western Sahara'
-            AND lg3.admin0_grid_id NOT IN (100314737,100083318,100041128,100133112,100341242,100132648,100222839,100379914,100055707,100379993,100130389,100255271,100363975,100248845,100001527,100342458,100024289,100132795,100054605,100253456,100342975,100074571)
-
-			UNION ALL
-            --
+        $results = $wpdb->get_results( "
+            SELECT lg.grid_id, lg.population, lg.country_code
+            FROM $wpdb->dt_location_grid lg
+            WHERE lg.level = 0 AND lg.grid_id IN ( " . dt_array_to_sql( $country_only ) . " )
             # admin 1 for little highly divided countries (352)
-            --
-            SELECT
-            lg4.grid_id, lg4.population,  lg4.country_code
-            FROM $wpdb->dt_location_grid lg4
-            WHERE lg4.level = 1
-            #'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
-            AND lg4.admin0_grid_id NOT IN (100050711,100219347, 100089589,100074576,100259978,100018514)
-            #'Romania', 'Estonia', 'Bhutan', 'Croatia', 'Solomon Islands', 'Guyana', 'Iceland', 'Vanuatu', 'Cape Verde', 'Samoa', 'Faroe Islands', 'Norway', 'Uruguay', 'Mongolia', 'United Arab Emirates', 'Slovenia', 'Bulgaria', 'Honduras', 'Columbia', 'Namibia', 'Switzerland', 'Western Sahara'
-            AND lg4.admin0_grid_id IN (100314737,100083318,100041128,100133112,100341242,100132648,100222839,100379914,100055707,100379993,100130389,100255271,100363975,100248845,100001527,100342458,100024289,100132795,100054605,100253456,100342975,100074571)
-
-			UNION ALL
-
- 			--
+            OR lg.level = 1 AND lg.admin0_grid_id IN ( " . dt_array_to_sql( $admin_1_countries ) . " ) AND lg.admin1_grid_id IS NOT NULL AND lg.admin2_grid_id IS NULL
             # admin 3 for big countries (6153)
-            --
-            SELECT
-            lg5.grid_id, lg5.population, lg5.country_code
-            FROM $wpdb->dt_location_grid as lg5
-            WHERE
-            lg5.level = 3
-            #'China', 'India', 'France', 'Spain', 'Pakistan', 'Bangladesh'
-            AND lg5.admin0_grid_id IN (100050711,100219347, 100089589,100074576,100259978,100018514)
-            #'Romania', 'Estonia', 'Bhutan', 'Croatia', 'Solomon Islands', 'Guyana', 'Iceland', 'Vanuatu', 'Cape Verde', 'Samoa', 'Faroe Islands', 'Norway', 'Uruguay', 'Mongolia', 'United Arab Emirates', 'Slovenia', 'Bulgaria', 'Honduras', 'Columbia', 'Namibia', 'Switzerland', 'Western Sahara'
-            AND lg5.admin0_grid_id NOT IN (100314737,100083318,100041128,100133112,100341242,100132648,100222839,100379914,100055707,100379993,100130389,100255271,100363975,100248845,100001527,100342458,100024289,100132795,100054605,100253456,100342975,100074571)
-
-			# Total Records (44395)
-
-       ", ARRAY_A );
-
+            OR lg.level = 3 AND lg.admin0_grid_id IN ( " . dt_array_to_sql( $admin_3_countries ) . " ) AND lg.admin3_grid_id IS NOT NULL AND lg.admin4_grid_id IS NULL
+            # admin 1 locations that have no level 2 (768)
+            OR lg.level = 1 AND lg.grid_id NOT IN ( SELECT lg22.admin1_grid_id FROM $wpdb->dt_location_grid lg22 WHERE lg22.level = 2 AND lg22.admin1_grid_id = lg.grid_id )
+            # admin 2 all countries (37100)
+            OR lg.level = 2 AND lg.admin0_grid_id NOT IN ( " . dt_array_to_sql( $admin_1_countries ) . " ) AND lg.admin0_grid_id NOT IN ( " . dt_array_to_sql( $admin_3_countries ) . " )
+            # Total Records (44395)
+        ", ARRAY_A);
         $list = [];
         if ( is_array( $results ) ) {
             foreach ( $results as $result ) {
